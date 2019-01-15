03493d2ddc527f1dfd11331f72a645eb27b4b07f
Haytham KATBY <mohakapt@gmail.com>
2019-01-15 13:37:41 +0300

    Reinitialize the library

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-/** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { NativeModules } from 'react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+const { RNNativeDialog } = NativeModules;
+
+export default RNNativeDialog;
